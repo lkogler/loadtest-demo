78461daa0efc7557c34d4bd55317af73b4e52841
Laura Kogler <lkogler@gmail.com>
2016-09-12 15:45:23 -0700

    Add fast and slow endpoints

--- main.py
+++ main.py
@@ -1,6 +1,13 @@
+import random
+import time
 from flask import Flask
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/fast')
+def fast():
+    return 'OK'
+
+@app.route('/slow')
 def hello_world():
-    return 'Hello, World!'
+    time.sleep(20 + random.random() * 5.0)
+    return 'OK'
