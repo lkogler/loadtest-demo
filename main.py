446c931ffca8a53d34dedeca01a08f8a3b5a5297
Laura Kogler <lkogler@gmail.com>
2016-09-14 16:58:09 -0700

    Changes to try to prevent out of memory errors

--- main.py
+++ main.py
@@ -1,19 +1,24 @@
 import random
 import time
 from flask import Flask
+
 app = Flask(__name__)
+
 
 @app.route('/fast')
 def fast():
     return 'OK'
+
 
 @app.route('/slow')
 def slow():
     time.sleep(20 + random.random() * 5.0)
     return 'OK'
 
+
 @app.route('/cpu')
 def cpu():
-    for x in range(300000000):
-        x*x
+    for i in range(3):
+        for x in range(100000000):
+            x * x
     return 'OK'
