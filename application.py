07f4a4bdba872f2fdd3559c24a7d8d01efee9c71
nate <nate91711@gmail.com>
2016-09-15 14:42:33 -0700

    Add configuration and change name of app to application for AWS EB deployment

--- application.py
+++ application.py
@@ -2,21 +2,21 @@ import random
 import time
 from flask import Flask
 
-app = Flask(__name__)
+application = Flask(__name__)
 
 
-@app.route('/fast')
+@application.route('/fast')
 def fast():
     return 'OK'
 
 
-@app.route('/slow')
+@application.route('/slow')
 def slow():
     time.sleep(20 + random.random() * 5.0)
     return 'OK'
 
 
-@app.route('/cpu')
+@application.route('/cpu')
 def cpu():
     for i in range(3):
         for x in range(100000000):
