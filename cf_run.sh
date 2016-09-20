8d0ce09980dd9f2df89be010ad6cef1279dbb25a
nate <nate91711@gmail.com>
2016-09-20 12:21:31 -0700

    Add parameter for GUNICORN_WORKER_CLASS

--- cf_run.sh
+++ cf_run.sh
@@ -9,4 +9,9 @@ if [ -f ${ENVIRONMENT_VARIABLES_FILE} ]
     source env/bin/activate
 fi
 
-gunicorn --bind 0.0.0.0:$PORT wsgi --graceful-timeout=$GUNICORN_TIMEOUT --timeout=$GUNICORN_TIMEOUT --workers=$WORKER_PROCESSES
+gunicorn wsgi \
+--bind 0.0.0.0:$PORT \
+--graceful-timeout=$GUNICORN_TIMEOUT \
+--timeout=$GUNICORN_TIMEOUT \
+--workers=$GUNICORN_WORKER_PROCESSES \
+--worker-class=$GUNICORN_WORKER_CLASS
