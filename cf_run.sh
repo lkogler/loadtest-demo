365303fca0baa0f6f5771011ebb5c9e3d53583a9
nate <nate91711@gmail.com>
2016-09-20 10:04:11 -0700

    Update cf_run to work both locally and on cloud foundry.

--- cf_run.sh
+++ cf_run.sh
@@ -6,8 +6,7 @@ ENVIRONMENT_VARIABLES_FILE=.env
 if [ -f ${ENVIRONMENT_VARIABLES_FILE} ]
   then
     source ${ENVIRONMENT_VARIABLES_FILE}
+    source env/bin/activate
 fi
-
-source env/bin/activate
 
 gunicorn --bind 0.0.0.0:$PORT wsgi --graceful-timeout=$GUNICORN_TIMEOUT --timeout=$GUNICORN_TIMEOUT --workers=$WORKER_PROCESSES
